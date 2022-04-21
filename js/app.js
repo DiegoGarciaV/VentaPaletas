b9e921efdeb25576358a55bc865f21f67bd8808e
DiegoGarciaV <71858943+DiegoGarciaV@users.noreply.github.com>
2022-04-21 05:50:16 -0500

    Consumo de servicio de productos

--- js/app.js
+++ js/app.js
@@ -2,5 +2,24 @@ var app = angular.module("PaletasApp",[]);
 
 app.controller("controlador",function($scope){
 
+    var paletas = $scope;
+
+    paletas.catalogo = {};
+
+    paletas.getProductos = function(){
+
+        $.ajax({
+                    url: "http://192.168.100.54:8080/api/productos", 
+                    success: function(result){
+              
+                            paletas.catalogo = result;
+                            alert(paletas.catalogo);
+                            $apply();
+                    }
+                });
+    };
+
+    paletas.getProductos();
     
+
 });
